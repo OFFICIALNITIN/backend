74cdf8c22a4db0670ff295ec8fd719996cca81be
OFFICIALNITIN <official.nitin105@gmail.com>
2024-01-21 20:42:19 +0530

    Fix typo in getUserChannelProfile function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -309,7 +309,7 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     )
 })
 
-const  getUserChanneProfile = asyncHandler(async(req,res)=>{
+const  getUserChannelProfile = asyncHandler(async(req,res)=>{
     const { username } = req.params
 
     if(!username?.trim()){
@@ -438,7 +438,7 @@ export {
      updateAccountDetails,
      updateUserAvatar,
      updateUserCoverImage,
-     getUserChanneProfile,
+     getUserChannelProfile,
      getWatchHistory
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,7 @@ import {
     updateAccountDetails, 
     updateUserAvatar, 
     updateUserCoverImage,
-    getUserChanneProfile,
+    getUserChannelProfile,
     getWatchHistory
 } from '../controllers/user.controller.js';
 
@@ -41,9 +41,9 @@ router.route("/refresh-token").post( refreshAccessToken )
 router.route("/change-password").post( verifyJWT, changeCurrentPassword)
 router.route("/current-user").get( verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-router.route("/avatar").patch( verifyJWT, upload.single("/avatar") , updateUserAvatar)
-router.route("/cover-image").patch( verifyJWT, upload.single("/coverImage") ,updateUserCoverImage)
-router.route("/channel/:username").get( verifyJWT, getUserChanneProfile)
+router.route("/avatar").patch( verifyJWT, upload.single("avatar") , updateUserAvatar)
+router.route("/cover-image").patch( verifyJWT, upload.single("coverImage") ,updateUserCoverImage)
+router.route("/channel/:username").get( verifyJWT, getUserChannelProfile)
 router.route("/history").get( verifyJWT, getWatchHistory)
 
 export default router;  
